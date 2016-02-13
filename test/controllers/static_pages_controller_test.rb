6695bfdc4160e3c16669733c1136fe867d87a701
“Mikegit config --global user.email “mikecardona2015@gmail.com <mikecardona2015@gmail.com>
2016-02-12 21:09:48 -0800

    Add a Contact page (solves exercises 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,5 +19,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
 
+
+  test "should get help" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+
 	end
 end
